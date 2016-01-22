1c44641f2f4aeea154c7be717710fdb9ae20ba27
Little Choppe <piiy@163.com>
2016-01-23 00:00:11 +0800

    MiniGame
    a ball that pick up the cube

---
+++ Roll_a_Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	//offset记录的是摄像机与物体的距离
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () 
+	{
+		//由于物体开始时在原点，所以摄像机与物体的距离与摄像机的坐标相等
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll_a_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rigibody;
+	private int count;
+
+	void Start()
+	{
+		rigibody = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		SetText ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigibody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//若接触到立方体就把立方体隐身
+		if (other.gameObject.tag == "PickUps")
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetText();
+		}
+	}
+
+	void SetText()
+	{
+		countText.text = "Count:" + count.ToString ();
+		if (count >= 31)
+			winText.text = "You Win!";
+	}
+}
